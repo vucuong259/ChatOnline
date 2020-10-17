1e4cd8dafefb9be18987668a1a7cc7c82d4e552b
Vu Manh Cuong <vucuong259@gmail.com>
2020-10-18 00:06:09 +0700

    [1.0.0] Add static pages

---
+++ sh/env.sh
@@ -0,0 +1,7 @@
+#Config database enviroment variable
+export DB_CONNECTION='mongodb'
+export DB_PORT='27017'
+export DB_HOST='localhost'
+export DB_NAME='chat_online'
+export DB_USERNAME=''
+export DB_PASSWORD=''
---
+++ src/config/connectDb.js
@@ -0,0 +1,23 @@
+import mongoose from 'mongoose';
+import bluebird from 'bluebird';
+
+/* 
+  connect MongoDB
+*/
+let connectDb = () => {
+    mongoose.Promise = bluebird;
+
+    let DB_CONNECTION = 'mongodb';
+    let DB_PORT = '27017';
+    let DB_HOST = 'localhost';
+    let DB_NAME = 'chat_online';
+    let DB_USERNAME = '';
+    let DB_PASSWORD = '';
+    // mongodb://localhost:27017/chat_online
+    let URI = `${DB_CONNECTION}://${DB_HOST}:${DB_PORT}/${DB_NAME}`;
+    return mongoose.connect(URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    });
+}
+module.exports = connectDb;
