1aed3aebf6ed59133ce6ff146ae4d34673ea6270
Vu Manh Cuong <vucuong259@gmail.com>
2020-10-21 23:40:59 +0700

    [1.0.0] Login local account function

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,10 @@
 import express from 'express';
 import { home, auth } from './../controllers';
-import { authValid } from './../validation'
+import { authValid } from './../validation';
+import passport from 'passport';
+import initPassportLocal from './../controllers/passportController/local';
+// Init all passport
+initPassportLocal();
 let router = express.Router();
 /* 
   Init all routes
@@ -11,6 +15,12 @@ let initRoutes = (app) => {
     router.get('/login-register', auth.getLoginRegister);
     router.post('/register', authValid.register, auth.postRegister);
     router.get('/verify/:token', auth.verifyAccount);
+    router.post('/login', passport.authenticate('local', {
+        successRedirect: '/',
+        failureRedirect: '/login-register',
+        successFlash: true,
+        failureFlash: true
+    }));
     return app.use('/', router);
 };
 module.exports = initRoutes;
