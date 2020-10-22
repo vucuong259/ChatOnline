caa5153684cbd451f1aff27b7c69a656dcade289
Vu Manh Cuong <vucuong259@gmail.com>
2020-10-22 14:10:57 +0700

    [1.0.0] Login facebook

--- sh/env.sh
+++ sh/env.sh
@@ -13,3 +13,8 @@ export MAIL_USER=vucuong25994@gmail.com
 export MAIL_PASSWORD=Makc@123
 export MAIL_HOST=smtp.gmail.com
 export MAIL_PORT=587
+
+#Config facebook login app
+export FB_APP_ID=351751859425206
+export FB_APP_SECRET=e775a1252a6e4745a3d22296eb1edfde
+export FB_CALLBACK_URL=https://localhost:2509/auth/facebook/callback
--- src/server.js
+++ src/server.js
@@ -7,28 +7,63 @@ import connectFlash from 'connect-flash';
 import configSession from './config/session';
 import passport from 'passport';
 
-let app = express();
-// connect mongoose db
-connectDb();
-// ConfigSession
-configSession(app);
-// ConfigviewEngine
-configViewEngine(app);
-//Enable post data form request
-app.use(bodyParser.urlencoded({ extended: true }));
-
-//Enable flash message
-app.use(connectFlash());
-//Config passport Js
-app.use(passport.initialize());
-app.use(passport.session());
-// init all routes
-initRoutes(app);
-
-let host = 'localhost';
-let port = 2509;
-
-
-app.listen(port, host, () => {
-    console.log(`Starting at ${host}:${port}/`);
+import pem from 'pem';
+import https from 'https';
+pem.config({
+    pathOpenSSL: 'C:/Users/Vu Manh Cuong/AppData/Local/Programs/Git/usr/bin/openssl'
 })
+
+pem.createCertificate({ days: 1, selfSigned: true }, function(err, keys) {
+    if (err) {
+        throw err
+    }
+    let app = express();
+    // connect mongoose db
+    connectDb();
+    // ConfigSession
+    configSession(app);
+    // ConfigviewEngine
+    configViewEngine(app);
+    //Enable post data form request
+    app.use(bodyParser.urlencoded({ extended: true }));
+
+    //Enable flash message
+    app.use(connectFlash());
+    //Config passport Js
+    app.use(passport.initialize());
+    app.use(passport.session());
+    // init all routes
+    initRoutes(app);
+
+    let host = 'localhost';
+    let port = 2509;
+
+    https.createServer({ key: keys.serviceKey, cert: keys.certificate }, app).listen(port, host, () => {
+        console.log(`Starting at ${host}:${port}/`);
+    });
+})
+
+// let app = express();
+// // connect mongoose db
+// connectDb();
+// // ConfigSession
+// configSession(app);
+// // ConfigviewEngine
+// configViewEngine(app);
+// //Enable post data form request
+// app.use(bodyParser.urlencoded({ extended: true }));
+
+// //Enable flash message
+// app.use(connectFlash());
+// //Config passport Js
+// app.use(passport.initialize());
+// app.use(passport.session());
+// // init all routes
+// initRoutes(app);
+
+// let host = 'localhost';
+// let port = 2509;
+
+// app.listen(port, host, () => {
+//     console.log(`Starting at ${host}:${port}/`);
+// });
