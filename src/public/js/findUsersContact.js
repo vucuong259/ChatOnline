8564b952bfb2aaa4ea0e5d5251e80ae65cde247d
Vu Manh Cuong <vucuong259@gmail.com>
2020-11-12 13:51:27 +0700

    [1.0.0] remove request contact sent.

--- src/public/js/findUsersContact.js
+++ src/public/js/findUsersContact.js
@@ -16,7 +16,7 @@ function callFindUsers(element){
     $.get(`/contact/find-users/${keyword}`, function(data){
       $('#find-user ul').html(data);
       addContact(); //js/addContact.js
-      removeRequestContact(); //js/removeRequestContact.js
+      removeRequestContactSent(); //js/removeRequestContact.js
     });
   }
 }
